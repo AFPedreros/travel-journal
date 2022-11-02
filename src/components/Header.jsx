89815f2611f20dba1f73ebd9718e85b5eea28e8f
AFPedreros <felipe.pedreros94@gmail.com>
2022-11-01 19:06:57 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <i class="fa-solid fa-earth-americas fa-2xl"></i>
+            <h1>my travel journal</h1>
+        </header>
+    )
+}
