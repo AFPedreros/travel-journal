e68ecda40657497ef28a1ff97e08f4b75cea4608
AFPedreros <felipe.pedreros94@gmail.com>
2022-11-02 12:38:22 -0500

    added the articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react"
+import { nanoid } from "nanoid"
 import Article from "./components/Article"
 import Header from "./components/Header"
+import data from "../data.js"
 
 function App() {
+    const articles = data.map((article) => {
+        return (
+            <Article
+                img={article.img}
+                country={article.country.toUpperCase()}
+                name={article.name}
+                date={article.date}
+                text={article.text}
+                key={nanoid()}
+            />
+        )
+    })
+
     return (
         <>
             <Header />
-            <div className="articles-container">
-                <Article />
-            </div>
+            <div className="articles-container">{articles}</div>
         </>
     )
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,24 +1,17 @@
-export default function Article() {
+export default function Article(props) {
     return (
         <article>
             <div className="img-container">
-                <img
-                    className="article-img"
-                    src="https://source.unsplash.com/WLxQvbMyfas"
-                ></img>
+                <img className="article-img" src={props.img}></img>
             </div>
             <div className="info-container">
                 <h4>
-                    Japan <a href="">View on Google Maps</a>
+                    <i class="fa-solid fa-location-dot"></i>
+                    {props.country} <a href="">View on Google Maps</a>
                 </h4>
-                <h2>Monte Fuji</h2>
-                <p>12 Jan, 2021 - 24 Jan, 2021</p>
-                <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at
-                    3,776 meters (12,380 feet). Mount Fuji is the single most
-                    popular tourist site in Japan, for both Japanese and foreign
-                    tourists.
-                </p>
+                <h2>{props.name}</h2>
+                <p className="date">{props.date}</p>
+                <p className="info">{props.text}</p>
             </div>
         </article>
     )
